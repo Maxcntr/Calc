fc4341f9905af44795a6259e8c3787b1229dcf31
Maxcntr <vinni1987@mail.com>
2021-05-01 17:26:58 +0500

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,19 @@
+
+class Calculator {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Input");
+        RomNumber.syncOper();
+        System.out.println("Output");
+        RomNumber.confStrOne();
+        RomNumber.confStrTwo();
+        RomNumber.numberOneInt();
+        RomNumber.numberTwoInt();
+        RangeInteger.rangeInt1();
+        RangeInteger.rangeInt2();
+        RangeInteger.rangeInt3();
+        RangeInteger.rangeInt4();
+        RomNumber.result();
+        RomNumber.stringResult(RomNumber.result());
+        RomNumber.resultinfo();
+    }
+}
